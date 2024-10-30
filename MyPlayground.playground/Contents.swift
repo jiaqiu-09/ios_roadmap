af304b76263cd0fae4570d310fbc7cea4b98fe00
jiaqiu <jiaqiu.win@outlook.com>
2024-10-30 08:48:38 +0800

    add swift class demo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -53,3 +53,30 @@ func sum2(_ a: Int,_ b: Int) -> Int {
 }
 print(sum2(22, 30))
 
+class Employee {
+    var name = ""
+    var salary = 0
+    var role = ""
+    
+    func doWork() {
+        print("Hi my name is \(name) and I'm doing work")
+        salary += 1
+    }
+}
+
+
+class Manager: Employee {
+    var teamSize = 0
+    
+    override func doWork() {
+        print("I'm managing people")
+        salary += 2
+    }
+}
+
+
+var m = Manager()
+m.name = "Jason"
+m.teamSize = 10
+m.doWork()
+print(m.teamSize)
