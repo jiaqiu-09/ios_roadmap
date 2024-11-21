10752aa8a2e5e57fbad045452398ca05df86180c
jiaqiu <jiaqiu.win@outlook.com>
2024-11-21 08:54:21 +0800

    Core Audio demo added

---
+++ CoreAudioDemo/CoreAudioDemo/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  CoreAudioDemo
+//
+//  Created by Jason Qiu on 2024/11/20.
+//
+
+import UIKit
+import AVFoundation
+import MediaPlayer
+
+
+class ViewController: UIViewController {
+    var playButton: UIButton!
+    var stopButton: UIButton!
+    var audioPlayer: AVAudioPlayer?
+    var volumeSlider: UISlider!
+    var volumeView: MPVolumeView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupUI()
+        
+        if let url = Bundle.main.url(forResource: "sample", withExtension: "mp3") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.prepareToPlay()
+                volumeSlider.value = audioPlayer?.volume ?? 1.0
+            } catch {
+                print("Audio file loading failed: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func setupUI() {
+        playButton = UIButton(type: .system)
+        playButton.setTitle("Play", for: .normal)
+        playButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        playButton.addTarget(self, action: #selector(playAudio), for: .touchUpInside)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(playButton)
+        
+        stopButton = UIButton(type: .system)
+        stopButton.setTitle("Stop", for: .normal)
+        stopButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        stopButton.addTarget(self, action: #selector(stopAudio), for: .touchUpInside)
+        stopButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stopButton)
+        
+        volumeSlider = UISlider()
+        volumeSlider.minimumValue = 0.0
+        volumeSlider.maximumValue = 1.0
+        volumeSlider.value = 1.0
+        volumeSlider.addTarget(self, action: #selector(volumeChanged), for: .valueChanged)
+        volumeSlider.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(volumeSlider)
+        
+        volumeView = MPVolumeView()
+        volumeView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(volumeView)
+        
+
+        
+        NSLayoutConstraint.activate([
+            playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -30),
+            
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: playButton.bottomAnchor, constant: 20),
+            
+            volumeSlider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            volumeSlider.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 30),
+            volumeSlider.widthAnchor.constraint(equalToConstant: 250),
+            
+            volumeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            volumeView.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 90),
+            volumeView.widthAnchor.constraint(equalToConstant: 250),
+            volumeView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    @objc func playAudio() {
+        audioPlayer?.play()
+    }
+    
+    @objc func stopAudio() {
+        audioPlayer?.stop()
+    }
+    
+    @objc func volumeChanged() {
+        audioPlayer?.volume = volumeSlider.value
+    }
+
+}
+
